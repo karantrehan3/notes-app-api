1a1c38fae35e8b3f4b0dca6ec35d2d216ffc911c
Karan Trehan <karantrehan3@gmail.com>
2021-05-24 10:51:42 +0530

    Routes

--- routes/index.js
+++ routes/index.js
@@ -1,80 +1,116 @@
-const notes = require('../notes.js');
-const express = require('express');
+const notes = require("../src/notes.js");
+const express = require("express");
+const { schema, delSchema } = require("../validation/validation-schema.js");
+const {
+    validateAddModSchema,
+    validateDelSchema,
+} = require("../validation/validation.js");
 const router = express.Router();
 
 // Welcome Page
-router.get('/', (req, res) => {
-    res.send('<h1>Welcome to Notes App!</h1>');
+router.get("/", (req, res) => {
+    res.send("<h1>Welcome to Notes App!</h1>");
 });
 
 // Listing the Notes
-router.get('/list', (req, res) => {
-    notes.loadNotes().then((data) =>{
-        res.status(200).send(data);
-    }).catch((err) =>{
-        res.status(400).send(err);
-        console.log(err);
-    })
+router.get("/list", (req, res) => {
+    notes
+        .loadNotes()
+        .then((data) => {
+            res.status(200).send(data);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+            console.log(err);
+        });
 });
 
 // Adding the Notes
-router.post('/add', (req, res) => {
-    notes.addNotes(req.body.title, req.body.body, (flag, message)=>{
-        if(flag){
-            res.status(201).json({
-                message: message,
-                status: flag
+router.post("/add", schema, validateAddModSchema, (req, res) => {
+    notes
+        .addNotes(req.body.title, req.body.body)
+        .then((flag) => {
+            if (flag) {
+                res.status(201).json({
+                    message: "Note Added Successfully",
+                    status: flag,
+                });
+            } else {
+                res.status(409).json({
+                    error: "Note with the same title Exists, Cannot Add Notes with same titles!",
+                    status: flag,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err.message,
+                status: false,
             });
-        }
-        else{
-            res.status(409).json({
-                message: message,
-                status: flag
-            });
-        }
-    });
+        });
 });
 
 // Modifying a Note
-router.put('/modify', (req, res) => {
-    notes.modifyNote(req.body.title, req.body.body, (flag, message)=>{
-        if(flag){
-            res.status(200).json({
-                message: message,
-                status: flag
+router.put("/modify", schema, validateAddModSchema, (req, res) => {
+    notes
+        .modifyNote(req.body.title, req.body.body)
+        .then((flag) => {
+            if (flag) {
+                res.status(200).json({
+                    message: "Note Modified Successfully!",
+                    status: flag,
+                });
+            } else {
+                res.status(400).json({
+                    error:
+                        "Note with title: `" +
+                        req.body.title +
+                        "` not present!",
+                    status: flag,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err.message,
+                status: false,
             });
-        }
-        else{
-            res.status(400).json({
-                message: message,
-                status: flag
-            });
-        }
-    });
+        });
 });
 
 // Deleting a Note
-router.delete('/remove', (req, res) => {
-    notes.removeNote(req.body.title, (flag, message)=>{
-        if(flag){
-            res.status(200).json({
-                message: message,
-                status: flag
+router.delete("/remove", delSchema, validateDelSchema, (req, res) => {
+    notes
+        .removeNote(req.body.title)
+        .then((flag) => {
+            if (flag) {
+                res.status(200).json({
+                    message: "Note Removed Successfully!",
+                    status: flag,
+                });
+            } else {
+                res.status(400).json({
+                    error:
+                        "Note with title: `" +
+                        req.body.title +
+                        "` not present!",
+                    status: flag,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err.message,
+                status: false,
             });
-        }
-        else{
-            res.status(400).json({
-                message: message,
-                status: flag
-            });
-        }
-    });
+        });
 });
 
 // For accessing any other url, this will work
-router.get('*',(req, res)=>{
+router.get("*", (req, res) => {
     res.status(404).json({
-        message: "404 Page Not Found!"
+        error: "404 Page Not Found!",
+        status: false,
     });
 });
 
